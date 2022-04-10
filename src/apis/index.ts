5735fdfdde6a34bde8111aa856446c8d610768e4
Abhirup <abhidatta146@gmail.com>
2022-04-10 12:37:22 +0530

    Added: ShowDetails top section

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,10 @@
 import FetchHelper from '../utils/FetchHelper';
 
 const GET_SHOWS = 'https://api.tvmaze.com/shows';
+const GET_SHOW_DETAIL = 'https://api.tvmaze.com/shows';
 
 export const getTVShowsList = () => FetchHelper.get({ url: GET_SHOWS });
+export const getTVShowDetail = (id: number) =>
+    FetchHelper.get({ url: `${GET_SHOW_DETAIL}/${id}` });
+
 export default {};
--- src/components/ShowCardDetails/ShowCardDetails.tsx
+++ src/components/ShowCardDetails/ShowCardDetails.tsx
@@ -1,14 +1,52 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getTVShowDetail } from '../../apis';
+import { TVSHOWSINFO } from '../../typings/tvShowsTypes';
+import Styles from './ShowCardDetails.module.scss';
 
 const ShowCardDetails = () => {
     const params = useParams();
     const showId = params.id;
+    const [detail, setDetail] = useState<TVSHOWSINFO>({} as TVSHOWSINFO);
+    const fetchShowDetails = async () => {
+        try {
+            if (showId) {
+                const result = await getTVShowDetail(parseInt(showId, 10));
+                console.log(result);
+                setDetail(result);
+            }
+        } catch (e) {
+            setDetail({} as TVSHOWSINFO);
+        }
+    };
+    useEffect(() => {
+        if (showId) {
+            fetchShowDetails();
+        }
+    }, [showId]);
+    if (Object.keys(detail).length === 0) {
+        return null;
+    }
 
     return (
-        <h1>
-            ShowCardDetails
-            {showId}
-        </h1>
+        <main className={Styles.wrapper}>
+            <section className={Styles.topSection}>
+                <div className={Styles.imgDiv}>
+                    <img
+                        src={detail.image.original}
+                        alt="show"
+                        className={Styles.img}
+                    />
+                </div>
+                <div className={Styles.aboutShow}>
+                    <p className={Styles.name}>{detail.name}</p>
+                    <p
+                        className={Styles.summary}
+                        dangerouslySetInnerHTML={{ __html: detail.summary }}
+                    />
+                </div>
+            </section>
+        </main>
     );
 };
 
